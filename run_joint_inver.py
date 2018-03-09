ee3fc9bded4b84d805a62637092912e9486b9e28
Emily <foo@bar>
2018-03-09 08:39:08 -0500

    Small modification to master script to save things properly.

--- run_joint_inver.py
+++ run_joint_inver.py
@@ -10,6 +10,7 @@ import os
 import input_data
 import pstats
 import cProfile
+import shutil
 
 pr = cProfile.Profile()
 pr.enable()
@@ -25,12 +26,13 @@ rf_obs, swd_obs, all_lims = input_data.LoadObservations()
 while os.path.exists('output/'+save_name):
     save_name += '_'+str(rnd_sd)
 os.mkdir('output/'+save_name)
+shutil.copyfile('input_data.py','output/'+save_name+'/input_data.py')
 save_name = 'output/'+save_name+'/'+save_name
 out = pipeline.JointInversion(rf_obs, swd_obs, all_lims, max_it, rnd_sd,
                               save_name, 'Ps')
 
 pr.disable()
-s=open('ouput/'+save_name+'/profiletimes.txt','w')
+s=open(save_name+'/profiletimes.txt','w')
 sortby = 'cumulative'
 ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
 ps.print_stats()
