7dd1e0b65b4f31e29df8edae02a28a9c6322c2ae
Emily <foo@bar>
2018-03-09 08:17:53 -0500

    Updated scripts so that running and plotting are separate, and things are saved more neatly.

---
+++ output/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  9 07:38:31 2018
+
+@author: emily
+"""
+
--- plotting.py
+++ plotting.py
@@ -1,60 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar  5 14:23:28 2018
+Created on Fri Mar  9 07:33:21 2018
 
 @author: emily
 """
 
+save_name = 'MBEY_Ps'
+
+
 import pipeline
 import numpy as np
 import matplotlib.pyplot as plt
-#import os
-import pstats
-import cProfile
+import shutil
 
-pr = cProfile.Profile()
-pr.enable()
+shutil.copyfile('./output/'+save_name+'/input.py', './input_data.py')
 
+import input_data
 
-#def try_running():
-max_it=200000
-rnd_sd = 10
+rf_obs, swd_obs, all_lims = input_data.LoadObservations()
 
-save_name = 'MBEY_Ps'
-rf_obs = pipeline.RecvFunc(amp = np.array([0.073, 0.125, 0.102, 0.022,
-                       -0.027, -0.015, 0.013, 0.011, -0.004, 0.004, 0.032, 0.037,
-                       0.003, -0.037, -0.048, -0.020, 0.033, 0.080, 0.089, 0.056,
-                       0.012, -0.016, -0.014, 0.003, 0.013, 0.006, -0.003, -0.003,
-                       -0.000, -0.001, -0.004, -0.001, 0.007, 0.009, -0.003, -0.021,
-                       -0.031, -0.027, -0.014, -0.001, 0.010, 0.018, 0.014, -0.002,
-                       -0.017, -0.023, -0.024, -0.021, -0.011, -0.000, 0.004, 0.002,
-                       0.004, 0.012, 0.015, 0.006, -0.004, -0.000, 0.012, 0.018,
-                       0.011, -0.004, -0.011, -0.007, 0.002, 0.004, 0.001, -0.001,
-                       -0.002, -0.002, -0.003, -0.003, -0.003, -0.005, -0.009, -0.014,
-                       -0.017, -0.016, -0.020, -0.029, -0.032, -0.022, -0.004, 0.007,
-                       0.006, -0.001, -0.005, -0.007, -0.010, -0.015, -0.012, 0.001,
-                       0.014, 0.016, 0.008, -0.002, -0.011, -0.019, -0.017, -0.006,
-                       0.006, 0.012, 0.013, 0.013, 0.006, -0.014, -0.033, -0.024,
-                       0.009, 0.030, 0.018, -0.005, -0.011, -0.000, 0.003, -0.008,
-                       -0.013, -0.002, 0.016, 0.022]), dt = 0.25, ray_param = 0.06147,
-                        std_sc = 2)
-
-swd_obs = pipeline.SurfaceWaveDisp(period = np.array([9.0, 10.1, 11.6, 13.5,
-                        16.2, 20.3, 25.0, 32.0, 40.0, 50.0, 60.0, 80.0]),
-                        c = np.array([3.212, 3.215, 3.233, 3.288,
-                       3.339, 3.388, 3.514, 3.647, 3.715, 3.798, 3.847, 3.937]))
-
-all_lims = pipeline.Limits(
-        vs = (0.5,5.5), dep = (0,200), std_rf = (0,0.05),
-        lam_rf = (0.05, 0.5), std_swd = (0,0.15))
-
-#os.mkdir('output/'+save_name)
 save_name = 'output/'+save_name+'/'+save_name
-#out = pipeline.JointInversion(rf_obs, swd_obs, all_lims, max_it, rnd_sd,
-#                              save_name, 'Ps')
-
-#actual_model = pipeline.SaveModel(pipeline.MakeFullModel(model),out[1][:,0])
-#%%
 all_models = np.load(save_name+'_AllModels.npy')
 
 good_mods = all_models[:,np.where(all_models[0,]>0)[0]]
@@ -164,25 +129,19 @@ plt.plot(synth_swd.period, synth_swd.c, '-',color = '0.25', linewidth=2)
 plt.plot(swd_obs.period, swd_obs.c-0.025,  'r--', linewidth=1)
 plt.plot(swd_obs.period, swd_obs.c+0.025,  'r--', linewidth=1)
 plt.tight_layout()
-#%%
+
+
 misfits = np.load(save_name+'_Misfit.npy')
 nm = int(misfits.size/3)
 plt.figure(); plt.title("Mahalanobis distance (least squares misfit - phi)")
-plt.plot(np.log10(misfits[:nm])); 
+plt.plot(np.log10(misfits[:nm]));
 plt.ylim(0.9*np.log10(np.min(misfits[:nm])),
          1.1*np.log10(np.max(misfits[10:nm])))
 
 plt.figure(); plt.title("Likelihood of accepting new model - alpha(m|m0)")
-plt.plot(np.log10(misfits[nm+1:2*nm])); 
-plt.ylim(0.9*np.log10(np.min(misfits[nm+1:2*nm])), 
+plt.plot(np.log10(misfits[nm+1:2*nm]));
+plt.ylim(0.9*np.log10(np.min(misfits[nm+1:2*nm])),
          1.1*np.log10(np.max(misfits[nm+10:2*nm])))
 
 plt.figure(); plt.title('Acceptance Rate')
 plt.plot(misfits[-nm:]*100)
-#%%
-pr.disable()
-s=open('thingy4.txt','w')
-sortby = 'cumulative'
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-ps.print_stats()
-s.close()
