edb3c283ef75ac5ba59cc37660b334590a3da648
Emily <foo@bar>
2018-03-09 08:17:53 -0500

    Updated scripts so that running and plotting are separate, and things are saved more neatly.

---
+++ run_joint_inver.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 14:23:28 2018
+
+@author: emily
+"""
+
+import pipeline
+import os
+import input_data
+import pstats
+import cProfile
+
+pr = cProfile.Profile()
+pr.enable()
+
+
+#def try_running():
+max_it=200000
+rnd_sd = 10
+
+save_name = 'MBEY_Ps'
+rf_obs, swd_obs, all_lims = input_data.LoadObservations()
+
+while os.path.exists('output/'+save_name):
+    save_name += '_'+str(rnd_sd)
+os.mkdir('output/'+save_name)
+save_name = 'output/'+save_name+'/'+save_name
+out = pipeline.JointInversion(rf_obs, swd_obs, all_lims, max_it, rnd_sd,
+                              save_name, 'Ps')
+
+pr.disable()
+s=open('ouput/'+save_name+'/profiletimes.txt','w')
+sortby = 'cumulative'
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+s.close()
