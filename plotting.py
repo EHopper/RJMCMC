445d15d7bebd4e404dfa83a32e1773a4675401d1
Emily <foo@bar>
2018-03-14 07:22:42 -0400

    Tidied up some useless outputs, and changed pipeline so that it samples evenly in depth when birthing a layer.

--- plotting.py
+++ plotting.py
@@ -5,7 +5,7 @@ Created on Fri Mar  9 07:33:21 2018
 @author: emily
 """
 
-save_name = 'NKAL_Ps_scale5_10'#'MBEY_Sp_scale5_10'#'MBEY_Ps_scale5'
+save_name = 'NKAL_Sp_scale5_10'#'MBEY_Sp_scale5_10'#'MBEY_Ps_scale5'
 save_every = 100
 
 import pipeline
