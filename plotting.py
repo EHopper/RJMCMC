6712135a55bc96b0bca5a476b4fe3e059700466c
Emily Hopper <emily.hopper353@gmail.com>
2018-07-25 09:00:18 -0400

    update plotting

--- plotting.py
+++ plotting.py
@@ -5,9 +5,9 @@ Created on Fri Mar  9 07:33:21 2018
 @author: emily
 """
 
-save_name = 'KIPE_Both_1'#'MBEY_Sp_scale5_10'#'MBEY_Ps_scale5'
+save_name = 'KIPE_Both_10'#'MBEY_Sp_scale5_10'#'MBEY_Ps_scale5'
 fol_append = '_00000' #''
-save_every = 1
+save_every = 100
 
 
 import pipeline
@@ -27,7 +27,7 @@ all_models = np.load(save_name+'_AllModels.npy')
 
 good_mods = all_models[:,np.where(all_models[0,]>0)[0]]
 nit = good_mods.shape[1]
-nit_cutoff = int(nit/50)
+nit_cutoff = int(nit/100)
 good_mods = good_mods[:,-nit_cutoff:]
 mean_mod = np.median(good_mods, axis = 1)
 #mean_mod = mean_mod[0][:,0]
